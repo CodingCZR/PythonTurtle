4c66f483d163695e3ff4a9dabd6e520b68931ad0
Cesar Miranda <cesarmirandacx602@gmail.com>
2024-04-01 15:23:08 -0700

    new file called pyTurtleSquare.py which draws a square

---
+++ main.py
@@ -0,0 +1,12 @@
+from turtle import *
+
+# moving the direction of the turtle 
+forward(10)
+
+# right and left create angles 
+right(90)
+
+forward(100)
+
+# done() allows the window to not close
+done()
---
+++ pyTurtleSquare.py
@@ -0,0 +1,18 @@
+# Project makes a square using Python Turtle
+
+from turtle import *
+
+# moving the direction of the turtle 
+forward(50)
+
+# right and left pertain to angles
+right(90) 
+forward(50)
+right(90)
+forward(50)
+right(90)
+forward(50)
+right(90)
+
+# done() allows the window to not close
+done()
